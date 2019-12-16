e947417f2ff1a1b1d2a7a38a7ea9d66cce303024
unknown <pemeraldy@gmail.com>
2019-12-16 05:49:44 +0100

    very little progress so far

--- assets/js/map.js
+++ assets/js/map.js
@@ -40,28 +40,31 @@ let techMap,
 
     // ######## AJAX data Calls #########
     layerData = L.geoJSON.ajax('data/PPMV.geojson', {
-        'pointToLayer': dataMarker
+        'pointToLayer': dataMarker,
+        onEachFeature: popUpData
     }).addTo(techMap)
 
     let layerData2 = L.geoJSON.ajax('data/hospital.geojson', {
-        'pointToLayer': dataStyler
+        'pointToLayer': dataStyler,
+        onEachFeature: popUpData
     }).addTo(techMap)
     
     let lagosLGA = L.geoJSON.ajax('data/lagos_LGA.geojson', {
-        'pointToLayer': dataStyler
+        'pointToLayer': dataStyler,
+        onEachFeature: popUpData
     }).addTo(techMap)
 
     let States = L.geoJSON.ajax('data/Nigeria_states.geojson', {
-        'pointToLayer': dataStyler
+        'pointToLayer': dataStyler,
+        onEachFeature: stateAttribute
     }).addTo(techMap)
 
     let lga = L.geoJSON.ajax('data/Nigeria_LGAs.geojson', {
-        'pointToLayer': dataStyler
+        'pointToLayer': dataStyler,
+        style: styleOne
     }).addTo(techMap)
 
-    let nigeriaState = L.geoJSON.ajax('.geojson', {
-        'pointToLayer': dataMarker
-    }).addTo(techMap)
+    
     
     
     layerData.on('data:loaded', () =>{
@@ -69,7 +72,29 @@ let techMap,
     })
 
 
-    
+    ////////// Autocomplete search
+	function addDataToAutocomplete(newArr) {
+		arr = arr.concat(newArr);
+		
+		function uniques(arr) {
+			var a = [];
+			for (var i=0, l=arr.length; i<l; i++)
+				if (a.indexOf(arr[i]) === -1 && arr[i] !== '')
+					a.push(arr[i]);
+					x =a.sort();
+			return x;
+		}
+		
+		// The source 4 autocomplete.  https://api.jqueryui.com/autocomplete/#method-option
+		$( "#autocomplete" ).autocomplete("option", "source", uniques(arr));
+		$( "#autocomplete"  ).autocomplete( "option", "minLength", 2 ); // number of char. before making list
+		$( "#autocomplete" ).on( "autocompleteselect", function( event, ui ) {
+			updateLayer(ui.item.value);
+			polySelect(ui.item.label);
+			ui.item.value='';
+		});
+	}	///////////// Autocomplete search end
+	
 
 
     // ############### HEATMAP ######
@@ -211,10 +236,10 @@ let techMap,
       var div = L.DomUtil.create("div", "legend trans-open");
       div.innerHTML += `<h4>Legend</h4>`;
       div.innerHTML += `<div class='anchor'>&gt</div>`;
-      div.innerHTML += `<i style="background: #477AC2"></i><span>Lagos</span><br>`;
-      div.innerHTML += `<i style="background: #448D40"></i><span>Ibadan</span><br>`;
-      div.innerHTML += `<i style="background: #E6E696"></i><span>Kano</span><br>`;
-      div.innerHTML += `<i style="background: #E8E6E0"></i><span>Kaduna</span><br>`;
+      div.innerHTML += `<i style="background: #477AC2"></i><span>Hospitals</span><br>`;
+      div.innerHTML += `<i style="background: #448D40"></i><span>Wards</span><br>`;
+      div.innerHTML += `<i style="background: #E6E696"></i><span>LGA</span><br>`;
+      div.innerHTML += `<i style="background: #E8E6E0"></i><span>States</span><br>`;
       
     
       return div;
@@ -228,17 +253,54 @@ let techMap,
 
     mainSideBar.onAdd = function() {
       var div = L.DomUtil.create("div", "main-side-bar slide-left");
-      div.innerHTML += `<h4>Utilities</h4>`;
-      div.innerHTML += `<div class='anchor'>&lt</div>`;
-      div.innerHTML += `<i style="background: #477AC2"></i><span>Lagos</span><br>`;
-      div.innerHTML += `<div class="query-continer">
-                            <a id="do" class="btn btn-primary" href="#"> click</a>
+    div.innerHTML += `<h4>Utilities</h4>`;
+    div.innerHTML += `<div class='anchor'>&lt</div>`;
+    div.innerHTML += `<i style="background: #477AC2"></i><span>Lagos</span><br>`;
+    div.innerHTML += `<div class="query-continer">
+                            <a id="do" class="btn btn-primary" href="#"> Remove layer</a>
                         </div>`;
-      
+    div.innerHTML += `<div class="query-continer">
+            <a id="remove" class="btn btn-primary" href="#"> add Layer</a>
+        </div>`;
+
+    div.innerHTML += `<div class="accordion" id="accordionExample">`
+    div.innerHTML += `<div class="card ">
+        <div class="card-header" id="headingOne">
+        <h2 class="mb-0">
+            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+            Buffer Area
+            </button>
+        </h2>
+        </div>
+
+        <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+        <div class="card-body">
+            Anim p
+        </div>
+        </div>
+    </div>`  
+        div.innerHTML += `<div class="card">
+        <div class="card-header" id="headingTwo">
+        <h2 class="mb-0">
+            <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+            Query Search
+            </button>
+        </h2>
+        </div>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+        <div class="card-body">
+            Buffer Area 
+        </div>
+        </div>
+    </div>`
+    div.innerHTML += `</div>`                         
+    div.innerHTML += `</div>`         //end div for query side bar                
+                       
+
       return div;
     };
     
-    mainSideBar.addTo(techMap);
+    mainSideBar.addTo(techMap); //Add created side bar to map
     
 
 
@@ -261,6 +323,7 @@ let techMap,
      
       techMap.on('click', function(e) {
         // let attr = json.properties
+      
             // console.log(e.target)
             // return L.circleMarker(json.latlng).bindTooltip(`<b>LGA:${attr.lga}</b> <br> 
             // Address: ${attr.address} <br> 
@@ -268,16 +331,33 @@ let techMap,
         });
 
 
+        
+
+
         /***************Search Works****************/
-        function returnProj(){
-            var layer = layerData2.getLayers()
-            console.log(layer.feature)
+        function returnProj(feat, layer){
+            techMap.removeLayer(layerData)
+        }
+
+        
+        function addLayer(feat, layer){
+            techMap.addLayer(layerData)
+            L.geoJSON(layerData, {
+                style: function (feature) {
+                    return {color: feature.properties.color};
+                },
+                filter: function(feature,layer){
+                    console.log(feature.properties)
+                }
+            })
+
         }
         let btn = document.getElementById('do').addEventListener('click', returnProj)
+        let btn2 = document.getElementById('remove').addEventListener('click', addLayer)
 
-        var searchLayer = L.layerGroup().addTo(techMap);
-        //... adding data in searchLayer ...
-        techMap.addControl( new L.Control.Search({layer: layerData}) );
+        // var searchLayer = L.layerGroup().addTo(techMap);
+        // //... adding data in searchLayer ...
+        // techMap.addControl( new L.Control.Search({layer: layerData}) );
 
        
        
@@ -291,12 +371,43 @@ let techMap,
         let attr = json.properties
         // console.log(attr)
         return L.circleMarker(latlng,{
-            color: 'orange',
+            color: 'red',
         }).bindTooltip(`<b>LGA:${attr.lga}</b> <br> 
         Address: ${attr.address} <br> 
         Wardcode: <i class="text-success">${attr.wardcode}</i>`)
     }
 
+    function highlightFeature(e) {
+        var layer = e.target;
+    
+        layer.setStyle({
+            weight: 2,
+            color: 'yellow',
+            dashArray: '',
+            fillOpacity: 0.4
+        });
+    
+        if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+            layer.bringToFront();
+        }
+    }
+
+    // function resetHighlight(e,geojson) {
+    //     // geojson = this.
+    //     // geojson.resetStyle(e.target);
+    // }
+    // function zoomToFeature(e,feature) {
+    //     console.log(this)
+    //     map.fitBounds(this._bonds._southWest.getBounds());
+    // }
+
+    function stateAttribute(feat, layer){
+        layer.on({
+            // mouseover: highlightFeature,
+            // mouseout: resetHighlight,
+            // click: zoomToFeature
+        }).bindTooltip(`<div class="card"> ${feat.properties.statename}</div>`)
+    }
 
     function dataStyler(json, latlng){
         let attr = json.properties
@@ -310,4 +421,17 @@ let techMap,
                 Wardcode: <i class="text-primary"> ${attr.wardcode}</i>
             </div>
         `)
+    }
+
+    function popUpData(feature, ltlng){
+        let res = []
+        if(feature.properties){
+            res.push(feature.name)
+            console.log('from popup data',feature.properties)
+        }
+    }
+    let styleOne = {
+        "color": "purple",
+        "weight": 1,
+        "opacity": 0.65
     }
