259335997f9c33b5bd123dd2070558a5bbb571a1
Umar <madugu01@gmail.com>
2019-12-19 23:33:27 +0100

    full integrated of map.js but commented autosearch section causing errors

--- assets/js/map.js
+++ assets/js/map.js
@@ -47,21 +47,8 @@ $(document).ready(function () {
     baseTopo = L.tileLayer.provider('OpenTopoMap')
     baseCartoDB = L.tileLayer.provider('CartoDB.DarkMatter')
 
-    baseLayers = {
-        "Open Street Map": leyerOSM,
-        "Base Topo Map": baseTopo,
-        "Carto DB": baseCartoDB,
-        "Water Color" : baseWaterColor
-    }
 
-    let drawnItm = new L.featureGroup()
-    // ######## AJAX data Call #########
-
-    // Draw controller
-    drawnItems = new L.FeatureGroup()
-    techMap.addLayer(drawnItems)
-    // console.log(drawnItems)
-
+    // ######## AJAX data Calls #########
     // Dataset layer
     layerData = L.geoJSON.ajax('features', {
         'pointToLayer': dataMarker,
@@ -132,6 +119,71 @@ $(document).ready(function () {
         'pointToLayer': dataMarker,
         'style': red,
     })
+
+
+    ////////// Autocomplete search
+    // let url = statesLayer,
+    //     arr = [], arr1 = []
+    // $('#autocomplete').autocomplete()
+
+    function style(feature) {
+        return {
+            fillColor: 'green',
+            fillOpacity: 0.5,
+            weight: 2,
+            opacity: 1,
+            color: '#ffffff',
+            dashArray: '3'
+        };
+    }
+
+    let highlight = {
+        'fillColor': 'yellow',
+        'weight': 2,
+        'opacity': 1
+    };
+
+    let stateLayer = L.geoJson(null, { onEachFeature: forEachFeature, style: style });
+
+    function forEachFeature(feature, layer) {
+        // Tagging each state poly with their name for the search control.
+        layer._leaflet_id = feature.properties.statename;
+
+        let popupContent = "<p><b>STATE: </b>" + feature.properties.statename +
+            "</br>REGION: " + feature.properties.statecode
+
+        layer.bindPopup(popupContent);
+
+        layer.on("click", function (e) {
+            stateLayer.setStyle(style); //resets layer colors
+            layer.setStyle(highlight);  //highlights selected.
+        });
+    }
+
+    $.getJSON(null, function (data) {
+        stateLayer.addData(data);
+
+        for (i = 0; i < data.features.length; i++) {  //loads State Name into an Array for searching
+            arr1.push({ label: data.features[i].properties.statename, value: "" });
+        }
+        addDataToAutocomplete(arr1);  //passes array for sorting and to load search control.
+    });
+
+    stateLayer.addTo(techMap);
+
+    // Draw controller
+    drawnItems = new L.FeatureGroup()
+    techMap.addLayer(drawnItems)
+    // console.log(drawnItems)
+    // let drawned = new L.FeatureGroup()
+    // drawned.addTo(techMap)
+
+    baseLayers = {
+        "Open Street Map": leyerOSM,
+        "Base Topo Map": baseTopo,
+        "Carto DB": baseCartoDB,
+        "Water Color": baseWaterColor
+    }
 
     overlays = {
         "States": statesLayer,
@@ -242,20 +294,37 @@ $(document).ready(function () {
 
     // #########LEGEND TEMPLATES ###########
 
+    // ######### Feature INfo Bar ###########
+
     /*Legend specific*/
-    let legend = L.control({ position: "bottomleft" });
+    legend = L.control({ position: "bottomleft" });
 
-    legend.onAdd = function (layerdata) {
-        var div = L.DomUtil.create("div", "legend trans-open");
-        div.innerHTML += `<h4>Legend</h4>`;
-        div.innerHTML += `<div class='anchor'>&gt</div>`;
-        div.innerHTML += `<i style="background: #477AC2"></i><span>Hospitals</span><br>`;
-        div.innerHTML += `<i style="background: #448D40"></i><span>Wards</span><br>`;
-        div.innerHTML += `<i style="background: #E6E696"></i><span>LGA</span><br>`;
-        div.innerHTML += `<i style="background: #E8E6E0"></i><span>States</span><br>`;
+    legend.onAdd = function (featPoint) {
+        let div = L.DomUtil.create("div", "legend trans-open");
+        if (featPoint == undefined) {
+            return false
+        } else {
+            div.innerHTML += `<h2>Legend</h2>`;
+            div.innerHTML += `<div class='anchor'><i class="fas fa-chevron-right"></i></div>`;
+            div.innerHTML += `<div class="legend-content">
+                            <div class="card" style="width: 18rem;">
+                                <img id="feat-img" class="card-img-top" src="" alt="pharmacy image">
+                                <div class="card-body">
+                                    <h2 class="card-title" id="feat-name">Name of City</h2>
+                                    <div class="card-text">
+                                        <p class="text-info">Address:<b id="feat-add"></b></p>
+                                        <button class="btn btn-info disabled btn-sm">Number</button>:<b id="feat-num"></b> <br><br>
+                                        <button class="btn btn-info disabled btn-sm">LGA</button>:<b id="feat-lga"></b> <br><br>
+                                        <button class="btn btn-info disabled btn-sm">State</button>: <span id="feat-state"></span> <br><br>
+                                        <span class="badge badge-info">Address again</span><br>
+                                    </div>
+                                </div>
+                            </div>
 
+                        </div>`
 
-        return div;
+            return div;
+        }
     };
 
     legend.addTo(techMap);
@@ -328,12 +397,6 @@ $(document).ready(function () {
         console.log('heu')
     })
 
-
-    let styleOne = {
-        "color": "purple",
-        "weight": 1,
-        "opacity": 0.65
-    }
 })
 
 
@@ -348,7 +411,7 @@ function dataMarker(json, latlng) {
             icon: iconPPMV,
         }).bindTooltip(`<b>LGA:${attr.lga}</b> <br>
         Address: ${attr.address} <br>
-        Wardcode: <i class="text-success">${attr.wardcode}</i>`,{direction: 'top'})
+        Wardcode: <i class="text-success">${attr.wardcode}</i>`, { direction: 'top' })
     } else {
         return L.marker(latlng, {
             icon: iconHospital,
@@ -382,7 +445,7 @@ function stateAttribute(feat, layer) {
     }).bindTooltip(`<div class="card"> ${feat.properties.statename}</div>`)
 }
 
-function dataStyler(json, latlng){
+function dataStyler(json, latlng) {
     let attr = json.properties
     // console.log(attr)
     return L.marker(latlng).bindTooltip(`
@@ -393,7 +456,7 @@ function dataStyler(json, latlng){
             <br>
             Wardcode: <i class="text-primary"> ${attr.wardcode}</i>
         </div>
-    `,{direction: 'top'})
+    `, { direction: 'top' })
 }
 
 function popUpData(feature, ltlng) {
@@ -404,6 +467,12 @@ function popUpData(feature, ltlng) {
         res.push(att.name)
         console.log('from popup data', res, feat)
     }
+}
+
+let styleOne = {
+    "color": "purple",
+    "weight": 1,
+    "opacity": 0.65
 }
 
 // ************ASIGN CUSTOM MARKERS*************
@@ -428,6 +497,77 @@ let iconLaboratory = L.divIcon({
     iconAnchor: [15, 42]
 });
 
+/*********CUSTOM FUNCTONS**********/
+
+// click marker
+let clickmark;
+
+// When you click on a circle, it calls the onMapClick function and passes the layers coordinates.
+// I grab the coords which are X,Y, and I need to flip them to latLng for a marker,
+function onMapClick(coords) {
+    console.log(coords);
+    let thecoords = coords.toString().split(',');
+    let lat = thecoords[1];
+    let lng = thecoords[0];
+    console.log('click mark', clickmark)
+    //if prior marker exists, remove it.
+    if (clickmark != undefined) {
+        techMap.removeLayer(clickmark);
+    };
+
+    clickmark = L.circleMarker([lat, lng], {
+        radius: 8,
+        //opacity: 1,
+        color: "yellow",
+        fillColor: "yellow",
+        fillOpacity: 0.8
+    }
+    ).addTo(techMap);
+}
+// end of code for click marker.
+
+function feat1(feature, layer) {
+
+    layer.on('click', e => {
+        let coords = e.target.feature.geometry.coordinates
+
+        if (feature.geometry.type == "MultiPolygon") {
+            console.log(feature)
+            document.querySelector('.legend').classList.remove('trans-open')
+            document.querySelector('.legend-content').innerHTML = `
+            <div class="card" style="width: 18rem;">
+            <img id="feat-img" class="card-img-top" src="" alt="pharmacy image">
+            <div class="card-body">
+                <h2 class="card-title" id="feat-name">${feature.properties.statename}</h2>
+                <div class="card-text">
+                    <p class="text-info">State:<b id="feat-add">${feature.properties.statename}</b></p>
+                    <button class="btn btn-info disabled btn-sm">Geo Zone</button>:<b id="feat-num"></b>${feature.properties.geozone} <br><br>
+                    <button class="btn btn-info disabled btn-sm">LGA</button>:<b id="feat-lga"></b> <br><br>
+                    <span class="badge badge-info">${feature.properties.statecode}</span><br>
+                </div>
+            </div>
+        </div>
+
+            `
+            // document.querySelector('.legend').classList.remove('trans-open')
+            // document.getElementById('feat-name').innerHTML = feature.properties.statename
+            // document.getElementById('feat-add').innerHTML = feature.properties.capcity
+            // document.getElementById('feat-num').innerHTML = feature.properties.geozone
+            // document.getElementById('feat-lga').innerHTML = feature.properties.statecode
+
+        } else {
+            onMapClick(coords)
+            document.querySelector('.legend').classList.remove('trans-open')
+            document.getElementById('feat-img').src = feature.properties.photo
+            document.getElementById('feat-name').innerHTML = feature.properties.name
+            document.getElementById('feat-add').innerHTML = feature.properties.address
+            document.getElementById('feat-num').innerHTML = feature.properties.phone_number
+            document.getElementById('feat-lga').innerHTML = feature.properties.lga
+            document.getElementById('feat-state').innerHTML = feature.properties.state
+        }
+
+    })
+}
 // save drawn items layer
 $(".save-map").click(function (e) {
     $.ajax({
